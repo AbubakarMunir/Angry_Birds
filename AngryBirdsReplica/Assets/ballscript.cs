913ba635727abbaf4dfc286d2eaa9928ca775803
AbubakarMunir <60841984+AbubakarMunir@users.noreply.github.com>
2021-02-24 21:39:09 +0500

    add-Project

---
+++ AngryBirdsReplica/Assets/ballscript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballscript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    bool Ispressed;
+    public float releasetime = 0.15f;
+    public float maxdragdstance = 2f;
+    public Rigidbody2D hookrb;
+    public GameObject next;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Ispressed==true)
+        {
+            Vector2 mpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector3.Distance(mpos,hookrb.position)<=maxdragdstance)
+            {
+                rb.position = mpos;
+            }
+            else
+            {
+                rb.position=((mpos - hookrb.position).normalized * maxdragdstance)+ hookrb.position;
+            }
+                    
+        }
+    }
+    private void OnMouseDown()
+    {
+        Ispressed = true;
+        rb.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        Ispressed = false;
+        rb.isKinematic = false;
+        StartCoroutine(release());
+    }
+    IEnumerator release()
+    {
+        yield return new WaitForSeconds(releasetime);
+        GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
+        yield return new WaitForSeconds(5f);
+        if(next!=null)
+        {
+            next.SetActive(true);
+        }
+        else
+        {
+            if(enemyscript.count>0)
+            {
+                Debug.Log("GAME LOST");
+            }
+            else
+            {
+                //load scene
+            }
+        }
+        
+    }
+}
---
+++ AngryBirdsReplica/Assets/enemyscript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class enemyscript : MonoBehaviour
+{
+    public float health = 2f;
+    public GameObject killingeffect;
+    public static int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.relativeVelocity.magnitude>1)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Instantiate(killingeffect,transform.position,Quaternion.identity);
+        Destroy(this.gameObject);
+        count--;
+        if (count == 0)
+        {
+            int s = SceneManager.GetActiveScene().buildIndex;
+            if (s < 2)
+            {
+                s++;
+                SceneManager.LoadScene(s);
+            }
+            else
+            {
+                Debug.Log("wonnn");
+            }
+                
+        }
+            
+            
+    }
+}
